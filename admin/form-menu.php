94b5c0c6d52b2c3224e9566ec55236cb983e7650
Püstə <puste.aghayeva@gmail.com>
2021-05-22 11:16:38 +0400

    Hometask(rəssamlar)

---
+++ admin/form-menu.php
@@ -0,0 +1,178 @@
+<?php
+    session_start();
+    if ($_SESSION['logged_in'] == 1) {
+        include '../config/config.php';
+        $post_type = (isset($_GET['id'])) ? 'edit':'create';
+
+        if($_SERVER['REQUEST_METHOD']='POST')
+        {
+            if(isset($_POST['post-type']) and !empty($_POST['post-type']) and $_POST['post-type'] == 'create')
+            {
+                $parent_id      = (isset($_POST['parent_id'])) ? intval($_POST['parent_id']) : 0;
+                $order_number   = (isset($_POST['order_number'])) ? intval($_POST['order_number']) : 0;
+                $status         = (isset($_POST['status'])) ? intval($_POST['status']) : 0;
+                $translation    = (isset($_POST['translation']))? $_POST['translation'] : [];
+
+                $insert_menu    = "INSERT INTO `menu`(`parent_id`,`order_number`,`status`) VALUES ('$parent_id','$order_number','$status')";
+                $result_insert  = mysqli_query($conn,$insert_menu);
+                if($result_insert)
+                {
+                    $menu_id = mysqli_insert_id($conn);
+                    foreach ($translation as $key=>$value){
+                        $insert_translation    = "INSERT INTO `menu_translation`(`menu_id`,`lang_id`,`name`,`slug`) VALUES ('$menu_id','$key','".$value['name']."','".$value['slug']."')";
+                        mysqli_query($conn,$insert_translation);
+                    }
+                }
+            }
+            else if(isset($_POST['post-type']) and !empty($_POST['post-type']) and $_POST['post-type'] == 'edit')
+            {
+                $parent_id      = (isset($_POST['parent_id'])) ? intval($_POST['parent_id']) : 0;
+                $order_number   = (isset($_POST['order_number'])) ? intval($_POST['order_number']) : 0;
+                $status         = (isset($_POST['status'])) ? intval($_POST['status']) : 0;
+                $translation    = (isset($_POST['translation']))? $_POST['translation'] : [];
+
+                $insert_menu    = "INSERT INTO `menu`(`parent_id`,`order_number`,`status`) VALUES ('$parent_id','$order_number','$status')";
+                $result_insert  = mysqli_query($conn,$insert_menu);
+                if($result_insert)
+                {
+                    $menu_id = mysqli_insert_id($conn);
+                    foreach ($translation as $key=>$value){
+                        $insert_translation    = "INSERT INTO `menu_translation`(`menu_id`,`lang_id`,`name`,`slug`) VALUES ('$menu_id','$key','".$value['name']."','".$value['slug']."')";
+                        mysqli_query($conn,$insert_translation);
+                    }
+                }
+            }
+        }
+?>
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <?php include 'includes/head.php'; ?>
+
+  <body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+      <?php include 'includes/sidebar.php'; ?>
+
+      <!-- Content Wrapper -->
+      <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+        <div id="content">
+
+          <?php include 'includes/topbar.php'; ?>
+
+
+          <!-- Begin Page Content -->
+          <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <h1 class="h3 mb-2 text-gray-800">Menyu əlavə et</h1>
+            <br>
+            <!-- DataTales Example -->
+            <form  action="form-menu.php" method="post" enctype="multipart/form-data">
+              <div class="row">
+                <div class="col-4 form-section">
+                  <br>
+                  <div class="form-group" >
+                    <label for="exampleFormControlSelect1" style="margin: 23px 0;">Ana kateqoriya</label>
+                    <select class="form-control" name="parent_id" id="exampleFormControlSelect1">
+                      <option value="0">Seç</option>
+                      <?php
+                      $select_sql = "SELECT * FROM `menu_translation` WHERE `lang_id`=1";
+                      $result = mysqli_query($conn, $select_sql);
+                      while ($row1 = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                      ?>
+                      <option value="<?= $row1['menu_id'];?>"><?=$row1['name'];?></option>
+                      <?php
+                      }
+                      ?>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Sıra nömrəsi</label>
+                    <input type="number" name="order_number" min="0" class="form-control" id="exampleFormControlInput1" placeholder="Sıra nömrəsi">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleFormControlSelect1">Status</label>
+                    <select class="form-control" name="status" id="exampleFormControlSelect1">
+                      <option value="0">Deaktiv</option>
+                      <option selected value="1">Aktiv</option>
+                    </select>
+                  </div>
+
+                </div>
+                <div class="col-8">
+                  <nav>
+                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                      <?php
+                      $select_sql  = "SELECT * FROM `languages` WHERE `status`=1";
+                      $result      = mysqli_query($conn, $select_sql);
+                      while ($row  = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                      ?>
+                        <a class="nav-item nav-link <?php if($row['code'] == 'az') echo 'active' ?>" id="nav-<?= $row['code']; ?>-tab" data-toggle="tab" href="#nav-<?= $row['code']; ?>" role="tab" aria-controls="nav-<?= $row['code']; ?>" aria-selected="true"><?= $row['name']; ?></a>
+                      <?php
+                      }
+                      ?>
+                    </div>
+                  </nav>
+                  <div class="tab-content" id="nav-tabContent">
+                    <?php
+                    $select_sql  = "SELECT * FROM `languages` WHERE `status`=1";
+                    $result      = mysqli_query($conn, $select_sql);
+                    while ($row  = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                    ?>
+                      <div class="tab-pane fade show <?php if ($row['code'] == 'az') echo 'active' ?>" id="nav-<?= $row['code']; ?>" role="tabpanel" aria-labelledby="nav-<?= $row['code']; ?>-tab">
+                        <div class="form-group">
+                          <label for="exampleFormControlInput1" class="form-label" style="margin-top: 20px;">Menyu elementi adı</label>
+                          <input type="text" class="form-control" name="translation[<?= $row['id'] ?>][name]" id="exampleFormControlInput1">
+                        </div>
+                        <div class="form-group">
+                          <label for="exampleFormControlInput1" class="form-label">Slug</label>
+                          <input type="text" name="translation[<?= $row['id'] ?>][slug]" class="form-control" id="exampleFormControlInput1">
+                        </div>
+
+                      </div>
+                    <?php
+                    }
+                    ?>
+                    </div>
+                    <input type="hidden" name="post-type" value="<?=$post_type;?>">
+                    <div class="form-group">
+                      <button type="submit" href="form-menu.php" class="btn btn-primary" style="margin-top:5%; margin-left: 40%; margin-right:50%; width: 10em;" value="Submit">Yadda saxla</button>
+                    </div>
+                  </div>
+                
+              </div>
+            </form>
+
+          </div>
+          <!-- /.container-fluid -->
+
+        </div>
+        <!-- End of Main Content -->
+
+        <?php include 'includes/content-footer.php'; ?>
+
+      </div>
+      <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <?php include 'includes/footer.php'; ?>
+
+
+  </body>
+
+  </html>
+
+<?php
+
+} else {
+  header('Location: login.php');
+}
+
+?>
---
+++ admin/slides.php
@@ -0,0 +1,126 @@
+<?php
+session_start();
+if($_SESSION['logged_in'] == 1)
+{
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'includes/head.php'; ?>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <?php include 'includes/sidebar.php'; ?>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <div id="content">
+
+               <?php include 'includes/topbar.php'; ?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-2 text-gray-800">Rəssamlar Siyahı</h1>
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Rəssamlar Siyahı</h6>
+                        </div>
+                        
+                        <div class="card-body">
+                           <div class="table-responsive">
+                              <div id="dataTable_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                                 <div class="row">
+                                 <div class="col-12 col-sm-6 col-md-8">
+                                            Siyahı
+                                        </div>
+                                        <div class="col-6 col-md-4">
+                                        
+                                            <a href="form-slides.php" ><img src="../uploads/create2.png" style="width: 30px; height:30px; margin:  20px; margin-left:80px;"></a>
+                                            <a href="#" ><img src="../uploads/delete7.png" style="width: 40px; margin:  20px;  margin-left:90px;"></a>
+                                            
+                                        </div>
+                                    <div class="col-sm-12">
+                                       <table class="table table-bordered dataTable" id="dataTable" width="100%" cellspacing="0" role="grid" aria-describedby="dataTable_info" style="width: 100%;">
+                                          <thead>
+                                             <tr role="row">
+                                               
+                                                <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="Position: activate to sort column ascending" style="width: 38px;">Ad</th>
+                                                <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="Position: activate to sort column ascending" style="width: 38px;">Soyad</th>
+                                                <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="Office: activate to sort column ascending" style="width: 288px;">Şəkil</th>
+                                                <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="Office: activate to sort column ascending" style="width: 288px;">Əsər</th>
+                                                <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="Start date: activate to sort column ascending" style="width: 179px;">Məlumat</th>
+                                                <th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="Salary: activate to sort column ascending" style="width: 163px;">Əməliyyatlar</th>
+                                             </tr>
+                                          </thead>
+                                          <tbody>
+                                          <?php
+                                                include '../config/config.php';
+                                                include '../config/vars.php';
+                                                $select_sql = "SELECT * FROM orient_ressamlar.slider_translation xy 
+                                                               INNER JOIN orient_ressamlar.slider x ON xy.slider_id=x.id 
+                                                               Where xy.lang_id = 1 and x.status=1;";
+                                                $result     = mysqli_query($conn,$select_sql);
+                                                while ($row = mysqli_fetch_array($result,  MYSQLI_ASSOC)) {
+                                                    ?>
+                                                <tr role="row" class="even">
+                                                
+                                                <td><?=$row['name'];?></td>
+                                                <td><?=$row['surname'];?></td>
+                                                <td><img src="../uploads/<?=$row['image'];?>" class="img-thumbnail" width="150px" height="150px"></td>
+                                                <td class="sorting_1"><?=$row['text'];?></td>               
+                                                <td><?=$row['about'];?></td>
+
+                                                <td>
+                                                  <a href="edit-news.php?news=<?=$row['id'];?>"   class="btn btn-success">Redakte et</a>
+                                                  <a href="delete-news.php?news=<?=$row['id'];?>" class="btn btn-danger">Sil</a>
+                                                </td>
+                                                
+                                             </tr>
+                                             <?php
+                                                }
+                                             ?>
+                                             
+                                             
+                                          </tbody>
+                                       </table>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+
+            <?php include 'includes/content-footer.php'; ?>
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <?php include 'includes/footer.php'; ?>
+
+
+</body>
+
+</html>
+
+<?php
+
+} else {
+  header('Location: login.php');
+}
+
+?>
